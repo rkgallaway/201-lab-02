6bad3a862fa0391e3d57110d4b1c1a85b9da13cf
Ryan Gallaway <rkgallaway@msn.com>
2020-10-10 11:22:19 -0700

    meaningful message

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello world!');
+var username = prompt('What is your name?');
+console.log('Hi ' + username);
+
+// Ryan - Question 1:
+var homeLocation = prompt('Am I from Seattle?').toLowerCase();
+
+if(homeLocation === 'yes' || homeLocation === 'y' ){
+  // console.log('Yes!  I live in Seattle');
+  alert('Yes!  I live in Seattle');
+}
+
+var banana = 'rt';
+
